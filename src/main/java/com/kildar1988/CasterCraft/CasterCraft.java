1246a41e317967ca893fcec32413ba89a24f0fc9
Kildar1988 <kildar1988@gmail.com>
2015-03-27 22:25:34 -0500

    basic setup

---
+++ src/main/java/com/kildar1988/CasterCraft/CasterCraft.java
@@ -0,0 +1,14 @@
+package com.kildar1988.CasterCraft;
+
+import cpw.mods.fml.common.Mod;
+
+/**
+ * Created by Robert Bodily III on 3/25/2015.
+ */
+
+
+@Mod(modid="CasterCraft", name="CasterCaft", version="1.7.10-1.0")
+public class CasterCraft
+{
+
+}
